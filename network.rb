c22990cad48e6f4b0669f3a7e1638cca28fa3f4b
Adam Dalton <adam.dalton@gmail.com>
2012-07-12 16:27:51 -0400

    improved ui

--- network.rb
+++ network.rb
@@ -1,34 +1,73 @@
 Shoes.app do
-  @status_colors = {"500" => red, "200" => green, "404" => yellow}
-  background white
-  @list = stack do
-    para "Address traffic to:", :margin => [10, 8, 10, 0]
-    flow :margin => 10 do
 
+  @status_colors = {"500" => lightcoral, "200" => lightgreen, "404" => khaki}
+  @status_counts = {"2" => 0, "3" => 0, "4" => 0, "5" => 0}
+
+  stack(width: 450) do
+
+    para "Status Counts"
+    flow do
+      @success = para "Successes: #{@status_counts["2"]}"
+      @redirection = para "Redirections: #{@status_counts["3"]}"
+      @client_error = para "Client Errors: #{@status_counts["4"]}"
+      @server_error = para "Server Errors: #{@status_counts["5"]}"
+    end
+  end
+  stack do
+    flow do
       @url = edit_line :width => -120
-      button "Download", :width => 120 do
-
-        @statuses = stack do
-          every(2) do
-            @statuses.prepend do
-              background "#eee".."#ccd"
-              @status = stack :margin => 10 do
-                dl = nil
-                para @url.text, " [", link("cancel") { dl.abort }, "]", :margin => 0
-                d = inscription "Beginning transfer.", :margin => 0
-                p = progress :width => 1.0, :height => 14
-                dl = download @url.text, :progress => proc { |dl|
-                  d.text = "Transferred #{dl.transferred} of #{dl.length} bytes (#{dl.percent}%)"
-                  p.fraction = dl.percent * 0.01 },
-                              :finish => proc { |dl|
-                                d.text = "Download completed"
-                                @status.background @status_colors[dl.response.status.to_s]
+      @url.text = "http://www.ihmc.us"
+      button "Start", :width => 120 do
+        @traffic = every(1) do
+          @statuses.prepend do
+            @status = stack do
+              dl = nil
+              para @url.text, " [", link("cancel") { dl.abort }, "]", :margin => 0
+              d = inscription "Beginning transfer.", :margin => 0
+              p = progress :width => 1.0, :height => 14
+              dl = download @url.text, :progress => proc { |dl|
+                d.text = "Transferred #{dl.transferred} of #{dl.length} bytes (#{dl.percent}%)"
+                p.fraction = dl.percent * 0.01 },
+                            :finish => proc { |dl|
+                              @status.clear {
+                                status = dl.response.status.to_s
+                                background @status_colors[status]
+                                para @url.text
+                                inscription "Download completed. #{dl.percent}% Status: #{status}"
+                                p = progress width: 1.0, height: 14
+                                sc = @status_counts[status[0]] || 0
+                                sc += 1
+                                @status_counts[status[0]] = sc
+                                refresh_counts
                               }
-              end
+                            }
+
             end
           end
         end
       end
+
+      button "Stop", width: 120 do
+        @traffic.stop
+      end
+    end
+
+
+
+    background white
+    @list = stack do
+      @statuses = stack do
+
+      end
     end
   end
+
+  def refresh_counts
+    @success.text = "Successes: #{@status_counts["2"]}"
+    @redirection.text =  "Redirections: #{@status_counts["3"]}"
+    @client_error.text =  "Client Errors: #{@status_counts["4"]}"
+    @server_error.text =  "Server Errors: #{@status_counts["5"]}"
+
+  end
+
 end
